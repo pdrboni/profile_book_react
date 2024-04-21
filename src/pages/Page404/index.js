2c58b73d85c3790c79c4633f2b8ba841c763031b
pedrobg2707@gmail.com <pedrobg2707@gmail.com>
2024-04-21 13:09:18 -0300

    page 404

--- src/pages/Page404/index.js
+++ src/pages/Page404/index.js
@@ -5,7 +5,7 @@ import { Container } from '../../styles/GlobalStyles';
 export default function Page404() {
   return (
     <Container>
-      <h1>THIS PAGE DON'T EXIST</h1>
+      <h1>THIS PAGE DONT EXIST</h1>
     </Container>
   );
 }
